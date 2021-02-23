f1bd228fa861837f145893604a385d5e6507f64b
frdrcpeter007 <frdrcpeter@gmail.com>
2021-02-23 09:34:36 +0100

    Basic function and type return

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log("Test123");
+var greet;
+//greet = 'hello';
+greet = function () {
+    console.log("Hello world !");
+};
+var add = function (a, b, c) {
+    if (c === void 0) { c = 100; }
+    console.log(a + b);
+    console.log(c);
+};
+add(3, 98);
+var minus = function (a, b) {
+    return a - b;
+};
+var result = minus(30, 21);
+console.log(result);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,2 +1,22 @@
-console.log("Test123");
+let greet: Function;
 
+//greet = 'hello';
+
+greet = () => {
+    console.log("Hello world !");
+    
+}
+
+const add = (a: number, b: number, c: number | string = 100): void => {
+    console.log(a + b);
+    console.log(c);   
+}
+
+add(3, 98)
+
+const minus = (a: number, b: number): number => {
+    return a - b
+}
+
+let result = minus(30, 21)
+console.log(result);
