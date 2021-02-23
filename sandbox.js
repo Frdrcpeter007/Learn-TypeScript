5be139a9c125308cd9fc1a372a45350f1c065948
frdrcpeter007 <frdrcpeter@gmail.com>
2021-02-23 08:25:35 +0100

    Types Object and Array

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,26 @@
-var char = 'Luigi';
-var age = 28;
-var isYoung = true;
-//char = 30;
-char = "Mario";
-var circ = function (diameter) {
-    return diameter * Math.PI;
+//Array
+var names = ['luigi', 'mario'];
+names.push('yoshi');
+//names.push(3);
+//names[0] = 34;
+var mixed = ['Peter', 20, true];
+mixed.push('Frdrcpeter');
+mixed.push(5);
+mixed.push(false);
+mixed[0] = 3;
+//Object
+var ninja = {
+    name: 'Ken',
+    age: 34
 };
-console.log(circ(34));
+//ninja.age = "23";
+ninja.age = 23;
+//ninja.skills = ["HTML"];
+//Il faut respecter la structure de départ
+// ninja = {
+//     name: 'abcd'
+// }
+ninja = {
+    name: 'Master',
+    age: 23
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,12 +1,34 @@
-let char = 'Luigi';
-let age = 28;
-let isYoung = true;
+//Array
+let names = ['luigi', 'mario']
 
-//char = 30;
-char = "Mario";
+names.push('yoshi');
+//names.push(3);
+//names[0] = 34;
 
-const circ = (diameter: number) => {
-    return diameter * Math.PI
+let mixed = ['Peter', 20, true];
+
+mixed.push('Frdrcpeter');
+mixed.push(5)
+mixed.push(false)
+mixed[0] = 3;
+
+//Object
+let ninja = {
+    name: 'Ken',
+    age: 34
 }
 
-console.log(circ(34));
+//ninja.age = "23";
+ninja.age = 23;
+
+//ninja.skills = ["HTML"];
+
+//Il faut respecter la structure de départ
+// ninja = {
+//     name: 'abcd'
+// }
+
+ninja = {
+    name: 'Master',
+    age : 23
+}
