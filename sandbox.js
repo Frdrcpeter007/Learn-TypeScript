0d23e678fe5780c47c09b898b01bdd983fcdf1e4
frdrcpeter007 <frdrcpeter@gmail.com>
2021-02-23 09:02:25 +0100

    Any type

--- sandbox.js
+++ sandbox.js
@@ -1,30 +1,8 @@
-//Explicit type
-var char;
-var age;
-var isLoggedIn;
-//age = 'luigi'
-age = 34;
-//Array
-var ninjas;
-//ninjas = [23, 'abcd']
-ninjas = ['Ken', 'Master'];
-//Union Type
+//Any type
+var age = 25;
+age = "Salut";
+console.log(age);
 var mixed = [];
-//mixed.push(true)
-mixed.push('Peter');
-mixed.push(46);
-var uid;
-//uid = false;
-uid = '123';
-//Object
-var ninjaOne;
-ninjaOne = { name: 'abc', age: 22 };
-ninjaOne = [];
-var ninjaTwo;
-ninjaTwo = {
-    uid: "123",
-    name: 'Peter',
-    age: 99,
-    isDead: false
-};
-console.log(ninjaTwo);
+mixed.push({ name: 'luigi' });
+mixed.push(23);
+console.log(mixed);
