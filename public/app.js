bff1f20e00ca7122fe1b12dbb32709be0a06885d
frdrcpeter007 <frdrcpeter@gmail.com>
2021-02-25 07:46:11 +0100

    Finition de l'app

--- public/app.js
+++ public/app.js
@@ -1,35 +1,19 @@
 import { Invoice } from './classes/Invoce.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 import { Payment } from './classes/Payments.js';
-// let invOne = new Invoice("Peter NDENGO", "Développer un site web", 500),
-//     invTwo = new Invoice("Jonas NDENGO", "Créer une maquette maison", 630),
-//     paidOne = new Payment("ABCD", "Developpement d'un site web", 340);
-let invoices = [], payments = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
-// payments.push(paidOne);
-// console.log(invoices)
-// console.log(payments);
-// let docOne: HasFormatter,
-//     docTwo: HasFormatter;
-// docOne = new Invoice("Sael BATUTA", "Emmerdement", 300);
-// docTwo = new Payment("Pascal ABEDI", "Oppps", 130);
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-// console.log(docs);
 let form = document.querySelector('.new-item-form');
 //Input
-let type = document.querySelector("#type"), tofrom = document.querySelector("#tofrom"), details = document.querySelector("#details"), amount = document.querySelector("#amount"), item = document.querySelector(".item-list");
+let type = document.querySelector("#type"), tofrom = document.querySelector("#tofrom"), details = document.querySelector("#details"), amount = document.querySelector("#amount"), container = document.querySelector(".item-list"), item = new ListTemplate(container);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    let doc;
+    let doc, heading;
     if (type.value === 'invoice') {
+        heading = 'Facture d\'achat';
         doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
     }
     else {
+        heading = 'Payement';
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    let p = document.createElement('p');
-    p.innerHTML = doc.format();
-    item.appendChild(p);
+    item.render(doc, heading, 'end');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,7 @@
 import { Invoice } from './classes/Invoce.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 import { Payment } from './classes/Payments.js';
 import { HasFormatter } from './interfaces/HasFormatter.js';
-
-// let invOne = new Invoice("Peter NDENGO", "Développer un site web", 500),
-//     invTwo = new Invoice("Jonas NDENGO", "Créer une maquette maison", 630),
-//     paidOne = new Payment("ABCD", "Developpement d'un site web", 340);
-    let invoices: Invoice[] = [],
-        payments: Payment[] = [];
-
-// invoices.push(invOne);
-// invoices.push(invTwo);
-
-// payments.push(paidOne);
-
-// console.log(invoices)
-// console.log(payments);
-
-// let docOne: HasFormatter,
-//     docTwo: HasFormatter;
-
-// docOne = new Invoice("Sael BATUTA", "Emmerdement", 300);
-// docTwo = new Payment("Pascal ABEDI", "Oppps", 130);
-
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-
-// console.log(docs);
-
 
 let form = document.querySelector('.new-item-form')! as HTMLFormElement;
 
@@ -36,21 +10,24 @@ let type = document.querySelector("#type") as HTMLSelectElement,
     tofrom = document.querySelector("#tofrom") as HTMLInputElement,
     details = document.querySelector("#details") as HTMLInputElement,
     amount = document.querySelector("#amount") as HTMLInputElement,
-    item = document.querySelector(".item-list") as HTMLDivElement;
+    container = document.querySelector(".item-list") as HTMLUListElement,
+    item = new ListTemplate(container);
 
 form.addEventListener("submit", (e: Event) => {
     e.preventDefault();
 
-    let doc: HasFormatter;
+    let doc: HasFormatter,
+        heading: "Facture d'achat" | "Payement";
 
     if (type.value === 'invoice') {
+        heading = 'Facture d\'achat';
         doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
     } else {
+        heading = 'Payement';
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)   
     }
     
-    let p = document.createElement('p');
-    p.innerHTML = doc.format();
 
-    item.appendChild(p)
+    item.render(doc, heading, 'end')
+
 })
